9fa18d6144d72defe65b30c264a76ae26243aaa2
Ziad Fellahi <ziad.fellahi@outlook.fr>
2025-11-25 16:43:00 +0100

    Ajoutez des fichiers projet.

---
+++ Mission3/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Mission3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.GrpRecherche = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboDepartement = new System.Windows.Forms.ComboBox();
+            this.cboNom = new System.Windows.Forms.ComboBox();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.GrpRecherche.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.GrpRecherche);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(833, 443);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Gestion des médecins";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AllowDrop = true;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(841, 472);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(833, 443);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // GrpRecherche
+            // 
+            this.GrpRecherche.Controls.Add(this.cboNom);
+            this.GrpRecherche.Controls.Add(this.cboDepartement);
+            this.GrpRecherche.Controls.Add(this.label2);
+            this.GrpRecherche.Controls.Add(this.label1);
+            this.GrpRecherche.Location = new System.Drawing.Point(3, 3);
+            this.GrpRecherche.Name = "GrpRecherche";
+            this.GrpRecherche.Size = new System.Drawing.Size(827, 257);
+            this.GrpRecherche.TabIndex = 0;
+            this.GrpRecherche.TabStop = false;
+            this.GrpRecherche.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nom :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Département :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cboDepartement
+            // 
+            this.cboDepartement.FormattingEnabled = true;
+            this.cboDepartement.Location = new System.Drawing.Point(384, 18);
+            this.cboDepartement.Name = "cboDepartement";
+            this.cboDepartement.Size = new System.Drawing.Size(121, 24);
+            this.cboDepartement.TabIndex = 3;
+            // 
+            // cboNom
+            // 
+            this.cboNom.FormattingEnabled = true;
+            this.cboNom.Location = new System.Drawing.Point(72, 18);
+            this.cboNom.Name = "cboNom";
+            this.cboNom.Size = new System.Drawing.Size(121, 24);
+            this.cboNom.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 472);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.GrpRecherche.ResumeLayout(false);
+            this.GrpRecherche.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox GrpRecherche;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboNom;
+        private System.Windows.Forms.ComboBox cboDepartement;
+    }
+}
+
