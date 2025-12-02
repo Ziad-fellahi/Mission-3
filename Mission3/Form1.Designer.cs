48923e7c65b70d7a57ba6552d10a6d5e6abfb56d
Ziad Fellahi <ziad.fellahi@outlook.fr>
2025-12-02 14:53:41 +0100

    fait

--- Mission3/Form1.Designer.cs
+++ Mission3/Form1.Designer.cs
@@ -29,16 +29,22 @@
         private void InitializeComponent()
         {
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.GrpRecherche = new System.Windows.Forms.GroupBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.dgvMedecins = new System.Windows.Forms.DataGridView();
+            this.btnRechercher = new System.Windows.Forms.Button();
+            this.cboDepartement = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.GrpRecherche = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cboDepartement = new System.Windows.Forms.ComboBox();
-            this.cboNom = new System.Windows.Forms.ComboBox();
             this.tabPage1.SuspendLayout();
-            this.tabControl1.SuspendLayout();
             this.GrpRecherche.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedecins)).BeginInit();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabPage1
@@ -47,11 +53,116 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(833, 443);
+            this.tabPage1.Size = new System.Drawing.Size(1063, 592);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Gestion des médecins";
             this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // GrpRecherche
+            // 
+            this.GrpRecherche.Controls.Add(this.txtNom);
+            this.GrpRecherche.Controls.Add(this.btnSupprimer);
+            this.GrpRecherche.Controls.Add(this.btnModifier);
+            this.GrpRecherche.Controls.Add(this.btnAjouter);
+            this.GrpRecherche.Controls.Add(this.dgvMedecins);
+            this.GrpRecherche.Controls.Add(this.btnRechercher);
+            this.GrpRecherche.Controls.Add(this.cboDepartement);
+            this.GrpRecherche.Controls.Add(this.label2);
+            this.GrpRecherche.Controls.Add(this.label1);
+            this.GrpRecherche.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GrpRecherche.Location = new System.Drawing.Point(3, 3);
+            this.GrpRecherche.Name = "GrpRecherche";
+            this.GrpRecherche.Size = new System.Drawing.Size(1057, 586);
+            this.GrpRecherche.TabIndex = 0;
+            this.GrpRecherche.TabStop = false;
+            this.GrpRecherche.Text = "Recherche";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(76, 47);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 22);
+            this.txtNom.TabIndex = 10;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(264, 520);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(88, 23);
+            this.btnSupprimer.TabIndex = 9;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(164, 520);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 8;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(59, 520);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 7;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // dgvMedecins
+            // 
+            this.dgvMedecins.AllowUserToAddRows = false;
+            this.dgvMedecins.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMedecins.Location = new System.Drawing.Point(6, 96);
+            this.dgvMedecins.Name = "dgvMedecins";
+            this.dgvMedecins.ReadOnly = true;
+            this.dgvMedecins.RowHeadersWidth = 51;
+            this.dgvMedecins.RowTemplate.Height = 24;
+            this.dgvMedecins.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMedecins.Size = new System.Drawing.Size(1045, 401);
+            this.dgvMedecins.TabIndex = 6;
+            // 
+            // btnRechercher
+            // 
+            this.btnRechercher.BackColor = System.Drawing.Color.DarkGray;
+            this.btnRechercher.Location = new System.Drawing.Point(558, 48);
+            this.btnRechercher.Name = "btnRechercher";
+            this.btnRechercher.Size = new System.Drawing.Size(97, 23);
+            this.btnRechercher.TabIndex = 5;
+            this.btnRechercher.Text = "Rechercher";
+            this.btnRechercher.UseVisualStyleBackColor = false;
+            this.btnRechercher.Click += new System.EventHandler(this.btnRechercher_Click);
+            // 
+            // cboDepartement
+            // 
+            this.cboDepartement.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDepartement.FormattingEnabled = true;
+            this.cboDepartement.Location = new System.Drawing.Point(355, 47);
+            this.cboDepartement.MinimumSize = new System.Drawing.Size(4, 0);
+            this.cboDepartement.Name = "cboDepartement";
+            this.cboDepartement.Size = new System.Drawing.Size(121, 24);
+            this.cboDepartement.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Département :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nom :";
             // 
             // tabControl1
             // 
@@ -63,7 +174,7 @@
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(841, 472);
+            this.tabControl1.Size = new System.Drawing.Size(1071, 621);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage2
@@ -71,72 +182,25 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(833, 443);
+            this.tabPage2.Size = new System.Drawing.Size(1063, 592);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // GrpRecherche
-            // 
-            this.GrpRecherche.Controls.Add(this.cboNom);
-            this.GrpRecherche.Controls.Add(this.cboDepartement);
-            this.GrpRecherche.Controls.Add(this.label2);
-            this.GrpRecherche.Controls.Add(this.label1);
-            this.GrpRecherche.Location = new System.Drawing.Point(3, 3);
-            this.GrpRecherche.Name = "GrpRecherche";
-            this.GrpRecherche.Size = new System.Drawing.Size(827, 257);
-            this.GrpRecherche.TabIndex = 0;
-            this.GrpRecherche.TabStop = false;
-            this.GrpRecherche.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Nom :";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(287, 18);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 16);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Département :";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // cboDepartement
-            // 
-            this.cboDepartement.FormattingEnabled = true;
-            this.cboDepartement.Location = new System.Drawing.Point(384, 18);
-            this.cboDepartement.Name = "cboDepartement";
-            this.cboDepartement.Size = new System.Drawing.Size(121, 24);
-            this.cboDepartement.TabIndex = 3;
-            // 
-            // cboNom
-            // 
-            this.cboNom.FormattingEnabled = true;
-            this.cboNom.Location = new System.Drawing.Point(72, 18);
-            this.cboNom.Name = "cboNom";
-            this.cboNom.Size = new System.Drawing.Size(121, 24);
-            this.cboNom.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(841, 472);
+            this.ClientSize = new System.Drawing.Size(1071, 621);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabPage1.ResumeLayout(false);
-            this.tabControl1.ResumeLayout(false);
             this.GrpRecherche.ResumeLayout(false);
             this.GrpRecherche.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedecins)).EndInit();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -149,8 +213,13 @@
         private System.Windows.Forms.GroupBox GrpRecherche;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cboNom;
         private System.Windows.Forms.ComboBox cboDepartement;
+        private System.Windows.Forms.Button btnRechercher;
+        private System.Windows.Forms.DataGridView dgvMedecins;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.TextBox txtNom;
     }
 }
 
--- Mission3/Form1.cs
+++ Mission3/Form1.cs
@@ -7,47 +7,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure; // Parfois nécessaire aussi
 
-
-namespace Mission3
+namespace Mission3 // Vérifie que c'est bien le nom de ton projet
 {
     public partial class Form1 : Form
     {
+        // --- 1. CONNEXION BDD ---
+        private gsbrapports2016Entities monModele = new gsbrapports2016Entities();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
+        // --- 2. CHARGEMENT DE LA FENÊTRE ---
+        private void Form1_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                var lesDepartements = monModele.medecin
+                                        .Select(m => m.departement)
+                                        .Distinct()
+                                        .OrderBy(d => d)
+                                        .ToList();
+                cboDepartement.DataSource = lesDepartements;
+                cboDepartement.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
         }
 
-        private void GrpRecherche_Enter(object sender, EventArgs e)
+        // --- 3. BOUTON RECHERCHER ---
+        private void btnRechercher_Click(object sender, EventArgs e)
         {
+            string nomSaisi = txtNom.Text;
+            var requete = monModele.medecin.AsQueryable();
 
+            if (!string.IsNullOrEmpty(nomSaisi))
+            {
+                requete = requete.Where(m => m.nom.StartsWith(nomSaisi));
+            }
+
+            if (cboDepartement.SelectedIndex != -1)
+            {
+                int deptChoisi = Convert.ToInt32(cboDepartement.SelectedItem);
+                requete = requete.Where(m => m.departement == deptChoisi);
+            }
+
+            dgvMedecins.DataSource = requete.ToList();
+
+            // Masquer les colonnes inutiles si elles existent
+            if (dgvMedecins.Columns["rapport"] != null) dgvMedecins.Columns["rapport"].Visible = false;
+            if (dgvMedecins.Columns["departement1"] != null) dgvMedecins.Columns["departement1"].Visible = false;
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        // --- 4. BOUTON SUPPRIMER ---
+        private void btnSupprimer_Click(object sender, EventArgs e)
         {
+            if (dgvMedecins.SelectedRows.Count > 0)
+            {
+                var medecinSelectionne = (medecin)dgvMedecins.SelectedRows[0].DataBoundItem;
 
-        }
+                var confirmation = MessageBox.Show($"Voulez-vous vraiment supprimer {medecinSelectionne.nom} ?",
+                                                   "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+                if (confirmation == DialogResult.Yes)
+                {
+                    try
+                    {
+                        monModele.medecin.Remove(medecinSelectionne);
+                        monModele.SaveChanges();
+                        MessageBox.Show("Supprimé avec succès.");
 
-        }
+                        // On simule un clic sur rechercher pour rafraîchir la liste
+                        btnRechercher_Click(sender, e);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Impossible de supprimer ce médecin (il a des rapports liés).", "Erreur");
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
+                        // Astuce pour annuler la suppression en mémoire si ça a planté
+                        // (On recharge l'état initial des objets modifiés)
+                        var entitesModifiees = monModele.ChangeTracker.Entries()
+                            .Where(x => x.State != System.Data.Entity.EntityState.Unchanged).ToList();
+                        foreach (var entry in entitesModifiees) entry.Reload();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner une ligne.");
+            }
         }
     }
 }
